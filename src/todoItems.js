80547ace978422c7a77f27406c87d0aee15d7b42
Vperali <146312869+Vperali@users.noreply.github.com>
2024-10-09 02:41:20 -0500

    Update todoItems.js

--- src/todoItems.js
+++ src/todoItems.js
@@ -1,67 +1,9 @@
-const todos = [
-  { 
-    id: 0,
-    title: 'Todo 1', 
-    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-      aliquip ex ea commodo consequat. Duis aute irure dolor in
-      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-      culpa qui officia deserunt mollit anim id est laborum.`, 
-    dueDate: '2024-10-15' 
-  },
 
-  { 
-    id: 1,
-    title: 'Todo 2', 
-    description: `Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-      Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
-      Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis.`, 
-    dueDate: '2024-10-11' 
-  },
-
-  { 
-    id: 2,
-    title: 'Todo 3', 
-    description: `Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
-      Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. Vivamus nec nisi nec nunc mattis molestie. 
-      Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. Pellentesque habitant morbi tristique 
-      senectus et netus et malesuada fames ac turpis egestas.`, 
-    dueDate: '2024-10-09' 
-  },
-
-  { 
-    id: 3,
-    title: 'Todo 4', 
-    description: `Suspendisse potenti. Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. 
-      Vivamus nec nisi nec nunc mattis molestie. Sed auctor nunc nec nisi ultrices, in molestie nibh mattis. 
-      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse potenti. 
-      Nullam nec nibh nibh. Nullam quis nisl nec nunc congue mollis. `, 
-    dueDate: '2024-10-07' 
-  },
-
-  {
-    id: 4,
-    title: 'Buy Bus Ticket',
-    description: 'Buy a bus ticket to New York.',
-    dueDate: '2024-10-05'
-  },
-
-  {
-    id:256,
-    title: 'Groceries',
-    description: 'Apples, oranges, lemons, pears.',
-    dueDate: '2024-09-09'
-  },
-
-  {
-    id:257,
-    title: 'Clean bathroom',
-    description: 'Clean the bathroom.',
-    dueDate: '2024-11-21'
-  }
+const todoItems = [
+  { title: "Todo 1", description: "Description for Todo 1", dueDate: "2024-04-03" },
+  { title: "Todo 2", description: "Description for Todo 2", dueDate: "2024-04-05" },
+  { title: "Todo 3", description: "Description for Todo 3", dueDate: "2024-04-01" },
+  { title: "Todo 4", description: "Description for Todo 4", dueDate: "2024-04-07" },
 ];
 
-export {todos};
+export default todoItems;
